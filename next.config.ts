b1b6b3a632c04d3a790f80fb203a92cb8d13416c
hdkassamali <122648642+hdkassamali@users.noreply.github.com>
2025-06-16 02:15:21 -0700

    initial commit

---
+++ next.config.ts
@@ -0,0 +1,19 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/t/p/**',
+      },
+    ],
+  },
+  experimental: {
+    optimizePackageImports: ['react-icons'],
+  },
+};
+
+export default nextConfig;
